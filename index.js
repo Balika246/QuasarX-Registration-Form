ae17fa30d3b988c97f6348b5d8c0b871beb527e6
Balika246 <117373266+Balika246@users.noreply.github.com>
2024-01-14 15:22:17 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var form = document.getElementById("sheetdb-form");
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    fetch(form.action, {
-        method: "Post",
-        body: new FormData(document.getElementById("sheetdb-form")),
-    }).then(
-        response => response.json()
-    ).then((html) => {
-        window.open('main.html', '_blank');
+document.addEventListener("DOMContentLoaded", function () {
+    var form = document.getElementById("sheetdb-form");
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // Perform form submission logic here (e.g., sending data to server)
+        fetch(form.action, {
+            method: "POST",
+            body: new FormData(document.getElementById("sheetdb-form")),
+        }).then(
+            function (response) {
+                return response.json();
+            }
+        ).then(function (html) {
+            // Show the popup on successful form submission
+            document.querySelector(".popup").classList.add("active");
+        });
+    });
+
+    // Add an event listener to the dismiss button inside the popup
+    document.getElementById("dismiss-popup-btn").addEventListener("click", function () {
+        document.querySelector(".popup").classList.remove("active");
     });
 });
